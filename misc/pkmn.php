8e5fcd855ddf15e1a9b215589d59c8c4627c0a5c
Arrow Sparrow <dev@aroceu.cloud>
2024-12-05 18:46:01 -0500

    Update pkmn.php

--- misc/pkmn.php
+++ misc/pkmn.php
@@ -157,7 +157,7 @@ $rand_keys = array_rand($input, 2);
 echo $input[$rand_keys[0]] . " &/ ";
 echo $input[$rand_keys[1]] . "";
 ?>
-<br />
+<br /><br />
 <?php print_r(array_random($fandom)); ?>
 </div>
 
