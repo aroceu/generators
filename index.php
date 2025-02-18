1fdbb3ce2e47d170e295f79499353728ffc350b0
Arrow Sparrow <dev@aroceu.cloud>
2025-02-18 00:48:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,9 @@
 		</ul>
 		
 		<p>&copy; 2017-<?php echo date("Y"); ?></p>
-		<p>↑ ↑ ↓ ↓ ← → ← → B A</p>
+		<p style="letter-spacing: 15px;
+  text-shadow: 2px 2px 0 #ffe796;
+  margin-top: 30px;">↑ ↑ ↓ ↓ ← → ← → B A</p>
 	</div>
 
 	<script type="text/javascript" src="https://kingdra.net/x/konami.js"></script>
