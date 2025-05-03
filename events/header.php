d9d113ed44dc5257b3b2076632af45039037f2e0
aroceu <dev@aroceu.cloud>
2025-05-03 04:19:42 -0400

    Update header.php

--- events/header.php
+++ events/header.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>aroceu's event generator</title>
+    <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" type="image/x-icon" href="favico.png" />
 </head>
 
